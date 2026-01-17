b2851f683009b769ecb1713e21ad947b8c6423b1
meevivy01 <meevivy@gmail.com>
2026-01-17 08:55:42 +0700

    Update Git1.py

--- Git1.py
+++ Git1.py
@@ -95,8 +95,8 @@ if os.path.exists(CLIENTS_PATH):
 
 # --- TARGET CONFIG ---
 TARGET_UNIVERSITIES = ["ราชภัฏยะลา", "Yala Rajabhat"]  
-TARGET_FACULTIES = ["เครื่องสำอาง","Cosmetic Science"] 
-TARGET_MAJORS = ["เครื่องสำอาง", "วิทยาศาสตร์เครื่องสำอาง","Cosmetic Science", "Cosmetics", "Cosmetic"]
+TARGET_FACULTIES = ["เครื่องสำอาง","Cosmetic"] 
+TARGET_MAJORS = ["เครื่องสำอาง", "Cosmetics", "Cosmetic"]
 SEARCH_KEYWORDS = ["ราชภัฏยะลา เครื่องสำอาง","Cosmetic Yala Rajabhat"]
 
 # --- 🟢 เพิ่มชุดนี้ไว้ใต้ SEARCH_KEYWORDS ---
